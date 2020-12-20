e0591b730fce22d97cf8f389661726e3b122e3dd
DeadNews <uhjnnn@gmail.com>
2020-12-20 02:57:48 +0200

    init

---
+++ scripts/copy_time/main.lua
@@ -0,0 +1,22 @@
+require 'mp'
+
+local function set_clipboard(text)
+    mp.commandv("run", "/home/deadnews/.config/mpv/scripts/copy_time/copy_time.sh", text);
+end
+
+local function copy_time()
+    local time_pos = mp.get_property_number("time-pos")
+    local time_in_seconds = time_pos
+    local time_seg = time_pos % 60
+    time_pos = time_pos - time_seg
+    local time_hours = math.floor(time_pos / 3600)
+    time_pos = time_pos - (time_hours * 3600)
+    local time_minutes = time_pos/60
+    time_seg,time_ms=string.format("%.09f", time_seg):match"([^.]*).(.*)"
+    time = string.format("%02d:%02d:%02d.%s", time_hours, time_minutes, time_seg, time_ms)
+    mp.osd_message(string.format("Copied to Clipboard: %s", time))
+    set_clipboard(time)    
+end
+
+mp.add_key_binding("Ctrl+c", "copy_time", copy_time)
+
---
+++ scripts/powermizer.lua
@@ -0,0 +1,63 @@
+-- https://github.com/kevinlekiller/mpv_scripts/blob/master/powermizer/powermizer.lua
+
+--[[
+    https://github.com/kevinlekiller/mpv_scripts
+   
+    Sets "PowerMizer" in Nvidia GPUs with the proprietary driver to
+    "Maximum Performance" mode while mpv is playing.
+    Sets "PowerMizer" back to "Adaptive" when mpv is idle or exits.
+    
+    The script will try to automatically detect a GPU.
+    
+    You can also manually set your GPU like this: --script-opts=powermizer-gpu="[gpu:1]"
+    Find your GPU with this command: nvidia-settings -q gpus
+--]]
+
+-- local test = os.execute("which nvidia-settings > /dev/null")
+-- if (test == 0 or test == true) then
+--     local gpu = mp.get_opt("powermizer-gpu")
+--     if (gpu ~= nil) then
+--         gpu = string.match(gpu, "%[gpu:%d+%]")
+--     end
+--     if (gpu == nil) then
+--         local handle = assert(io.popen("nvidia-settings -q gpus"))
+--         for line in handle:lines() do
+--             gpu = string.match(line, "%[gpu:%d+%]")
+--             if (gpu ~= nil) then
+--                 break
+--             end
+--         end
+--         handle:close()
+--         if (gpu == nil) then
+--             gpu = "[gpu:0]"
+--         end
+--     end
+
+--     function switch(name, paused)
+--         -- If it's nil it's because of the "shutdown" event.
+--         if (paused == true or paused == nil) then
+--             os.execute("nvidia-settings -a " .. gpu .. "/GPUPowerMizerMode=0 > /dev/null")
+--         else
+--             os.execute("nvidia-settings -a " .. gpu .. "/GPUPowerMizerMode=1 > /dev/null")
+--         end
+--     end
+
+--     mp.observe_property("core-idle", "bool", switch)
+--     -- "core-idle" doesn't trigger when exiting mpv, use "shutdown" to set GPU back to adaptive.
+--     mp.register_event("shutdown", switch)
+-- end
+
+
+gpu = "[gpu:0]"
+function switch(name, paused)
+    -- If it's nil it's because of the "shutdown" event.
+    if (paused == true or paused == nil) then
+        os.execute("nvidia-settings -a " .. gpu .. "/GPUPowerMizerMode=0 > /dev/null")
+    else
+        os.execute("nvidia-settings -a " .. gpu .. "/GPUPowerMizerMode=1 > /dev/null")
+    end
+end
+
+mp.observe_property("core-idle", "bool", switch)
+-- "core-idle" doesn't trigger when exiting mpv, use "shutdown" to set GPU back to adaptive.
+mp.register_event("shutdown", switch)
