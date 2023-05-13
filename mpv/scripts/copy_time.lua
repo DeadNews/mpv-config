02ad04fb2935d0bd417bddd59ea8e50b33e6aca0
DeadNews <uhjnnn@gmail.com>
2023-05-14 04:00:18 +0530

    style: format code with `StyLua`

--- mpv/scripts/copy_time.lua
+++ mpv/scripts/copy_time.lua
@@ -1,10 +1,9 @@
-require 'mp'
+require("mp")
 
 function set_clipboard(text)
     -- for wayland
-    io.popen('wl-copy', 'w'):write(text):close()
+    io.popen("wl-copy", "w"):write(text):close()
 end
-
 
 local function copy_time()
     local time_pos = mp.get_property_number("time-pos")
@@ -13,7 +12,7 @@ local function copy_time()
     local time_hours = math.floor(time_pos / 3600)
     time_pos = time_pos - (time_hours * 3600)
     local time_minutes = time_pos / 60
-    time_seg, time_ms = string.format("%.09f", time_seg):match"([^.]*).(.*)"
+    time_seg, time_ms = string.format("%.09f", time_seg):match("([^.]*).(.*)")
     time = string.format("%02d:%02d:%02d.%s", time_hours, time_minutes, time_seg, time_ms)
     mp.osd_message(string.format("Copied to Clipboard: %s", time))
     set_clipboard(time)
