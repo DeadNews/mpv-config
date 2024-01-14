386a56a89a23418a0ad5663c83f986a3765d9a59
DeadNews <uhjnnn@gmail.com>
2024-01-14 21:31:01 +0700

    feat(scripts): update `slicing_copy`

--- mpv/scripts/copy_time.lua
+++ mpv/scripts/copy_time.lua
@@ -1,22 +1,52 @@
-require("mp")
+-- Copy the video's current time to the clipboard in HH:MM:SS.xxx format.
 
-function set_clipboard(text)
-    -- for wayland
-    io.popen("wl-copy", "w"):write(text):close()
+require("mp")
+local msg = require("mp.msg")
+local options = require("mp.options")
+
+local opts = {
+    copy_method = "",
+}
+options.read_options(opts)
+
+local function info(s)
+    msg.info(s)
+    mp.osd_message(s)
+end
+
+local function timestamp(duration)
+    local hours = math.floor(duration / 3600)
+    local minutes = math.floor(duration % 3600 / 60)
+    local seconds = duration % 60
+    return string.format("%02d:%02d:%06.3f", hours, minutes, seconds)
+end
+
+local function set_clipboard(text)
+    if opts.copy_method == "wayland" then
+        local pipe = io.popen("wl-copy", "w")
+        pipe:write(text)
+        pipe:close()
+    elseif opts.copy_method == "xclip" then
+        local pipe = io.popen("xclip -silent -in -selection clipboard", "w")
+        pipe:write(text)
+        pipe:close()
+    elseif opts.copy_method == "pbcopy" then
+        local pipe = io.popen("pbcopy", "w")
+        pipe:write(text)
+        pipe:close()
+    elseif opts.copy_method == "powershell" then
+        mp.commandv("run", "powershell", "set-clipboard", text)
+    else
+        info("copy_method not in wayland, xclip, pbcopy, powershell")
+    end
 end
 
 local function copy_time()
     local time_pos = mp.get_property_number("time-pos")
-    local time_seg = time_pos % 60
-    time_pos = time_pos - time_seg
-    local time_hours = math.floor(time_pos / 3600)
-    time_pos = time_pos - (time_hours * 3600)
-    local time_minutes = time_pos / 60
-    time_seg, time_ms = string.format("%.09f", time_seg):match("([^.]*).(.*)")
-    time = string.format("%02d:%02d:%02d.%s", time_hours, time_minutes, time_seg, time_ms)
+    local time = timestamp(time_pos)
 
-    mp.osd_message(string.format("Copied to Clipboard: %s", time))
     set_clipboard(time)
+    info(string.format("Copied to Clipboard: %s", time))
 end
 
-mp.add_key_binding("Ctrl+c", "copy_time", copy_time)
+mp.add_key_binding("F1", "copy_time", copy_time)
