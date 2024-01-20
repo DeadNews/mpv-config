26bb57ec48f459af9a05161316ffc9c79d2e4096
DeadNews <uhjnnn@gmail.com>
2024-01-21 00:37:14 +0200

    feat(scripts): update `copy_time` (#17)
    * feat(scripts): update `copy_time`
    * feat(scripts): rename `slicing_copy` to `slicing`
    * feat(scripts): update `autoload`
    * chore(makefile): update

--- Makefile
+++ Makefile
@@ -10,5 +10,5 @@ pc-run:
 	pre-commit run -a
 
 scripts-up:
-	wget --output-document mpv/scripts/autoload.lua https://raw.githubusercontent.com/mpv-player/mpv/master/TOOLS/lua/autoload.lua
-	wget --output-document mpv/scripts/slicing_copy.lua https://raw.githubusercontent.com/snylonue/mpv_slicing_copy/master/slicing_copy.lua
+	wget -O mpv/scripts/autoload.lua https://raw.githubusercontent.com/mpv-player/mpv/master/TOOLS/lua/autoload.lua
+	wget -O mpv/scripts/slicing.lua https://raw.githubusercontent.com/snylonue/mpv_slicing_copy/master/slicing_copy.lua
--- mpv/scripts/copy_time.lua
+++ mpv/scripts/copy_time.lua
@@ -1,22 +1,60 @@
-require("mp")
+-- Copy the video's current time to the clipboard in HH:MM:SS.xxx format.
+-- Default keybinding: F1
 
-function set_clipboard(text)
-    -- for wayland
-    io.popen("wl-copy", "w"):write(text):close()
+--[[
+To configure this script use file ~~/script-opts/copy_time.conf.
+
+Example configuration would be:
+
+mode=wayland|xclip|pbcopy|powershell
+--]]
+
+require("mp")
+local options = require("mp.options")
+
+local opts = {
+    mode = "",
+}
+options.read_options(opts)
+
+local function info(s)
+    mp.msg.info(s)
+    mp.osd_message(s)
+end
+
+local function timestamp(duration)
+    local hours = math.floor(duration / 3600)
+    local minutes = math.floor(duration % 3600 / 60)
+    local seconds = duration % 60
+    return string.format("%02d:%02d:%06.3f", hours, minutes, seconds)
+end
+
+local function set_clipboard(text)
+    if opts.mode == "wayland" then
+        local pipe = io.popen("wl-copy", "w")
+        pipe:write(text)
+        pipe:close()
+    elseif opts.mode == "xclip" then
+        local pipe = io.popen("xclip -silent -in -selection clipboard", "w")
+        pipe:write(text)
+        pipe:close()
+    elseif opts.mode == "pbcopy" then
+        local pipe = io.popen("pbcopy", "w")
+        pipe:write(text)
+        pipe:close()
+    elseif opts.mode == "powershell" then
+        mp.commandv("run", "powershell", "set-clipboard", text)
+    else
+        mp.msg.error("Invalid mode: ", opts.mode)
+    end
 end
 
 local function copy_time()
     local time_pos = mp.get_property_number("time-pos")
-    local time_seg = time_pos % 60
-    time_pos = time_pos - time_seg
-    local time_hours = math.floor(time_pos / 3600)
-    time_pos = time_pos - (time_hours * 3600)
-    local time_minutes = time_pos / 60
-    time_seg, time_ms = string.format("%.09f", time_seg):match("([^.]*).(.*)")
-    time = string.format("%02d:%02d:%02d.%s", time_hours, time_minutes, time_seg, time_ms)
+    local time = timestamp(time_pos)
 
-    mp.osd_message(string.format("Copied to Clipboard: %s", time))
     set_clipboard(time)
+    info(string.format("Copied to Clipboard: %s", time))
 end
 
-mp.add_key_binding("Ctrl+c", "copy_time", copy_time)
+mp.add_key_binding("F1", "copy_time", copy_time)
